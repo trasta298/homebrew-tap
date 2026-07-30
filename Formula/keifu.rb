4e52875937824d848aecf6f5be93688e491a33ad
trasta <trasta298@gmail.com>
2026-07-30 13:06:10 +0000

    🍺 Update keifu to 0.6.0

--- Formula/keifu.rb
+++ Formula/keifu.rb
@@ -1,28 +1,28 @@
 class Keifu < Formula
   desc "Git commit graph visualization in the terminal"
   homepage "https://github.com/trasta298/keifu"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/trasta298/keifu/releases/download/v0.5.0/keifu-v0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "474c9cce2d7450ccdc1fcb1c3dfe5119c24c311a9e9e41ecf05c51af58749b36"
+      url "https://github.com/trasta298/keifu/releases/download/v0.6.0/keifu-v0.6.0-aarch64-apple-darwin.tar.gz"
+      sha256 "22c175cd7beae02c7743387f641da1b9c930f8dc9c2566eb0c6d9ee640549cb9"
     end
     on_intel do
-      url "https://github.com/trasta298/keifu/releases/download/v0.5.0/keifu-v0.5.0-x86_64-apple-darwin.tar.gz"
-      sha256 "24cb6a6205cdff38560c7e340d1261b9e4a909cf0321c75d1cbc5e46bf4b1cb1"
+      url "https://github.com/trasta298/keifu/releases/download/v0.6.0/keifu-v0.6.0-x86_64-apple-darwin.tar.gz"
+      sha256 "e355c22b4a946ebac7e8ca610465e6ee59548d81cc872b1f0ec61d1e16a5336f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/trasta298/keifu/releases/download/v0.5.0/keifu-v0.5.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2349cba69c3b78c742bbfd8af567176290db5a9f36bc9e4cbede864b53df8692"
+      url "https://github.com/trasta298/keifu/releases/download/v0.6.0/keifu-v0.6.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e361563db635b91d2222cc109d6c6412438f9f8ca42f250e7d00974048752861"
     end
     on_intel do
-      url "https://github.com/trasta298/keifu/releases/download/v0.5.0/keifu-v0.5.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "14f977aff2e493731bc808e901a72b58064e1e7f3d759ef70d5dc219d8c2a15a"
+      url "https://github.com/trasta298/keifu/releases/download/v0.6.0/keifu-v0.6.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e604044094036d32e4e299f1e83403a9d9b272f89391c22bc9702860491bd4a8"
     end
   end
 
